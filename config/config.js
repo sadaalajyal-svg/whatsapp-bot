ec9cf3b183aaa3820e100cc31a21e7834d519af1
sadaalajyal-svg <sadaalajyal@gmail.com>
2026-07-31 09:48:45 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,1 +1,14 @@
 
+const config = {
+  PORT: process.env.PORT || 3000,
+
+  VERIFY_TOKEN: process.env.VERIFY_TOKEN,
+
+  WHATSAPP_TOKEN: process.env.WHATSAPP_TOKEN,
+
+  PHONE_NUMBER_ID: process.env.PHONE_NUMBER_ID,
+
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+};
+
+export default config;
