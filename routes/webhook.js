11d99b4fbbd978279b16f749110f782194bcc702
sadaalajyal-svg <sadaalajyal@gmail.com>
2026-07-31 09:50:53 +0300

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,1 +1,33 @@
 
+import express from "express";
+import config from "../config/config.js";
+
+const router = express.Router();
+
+// التحقق من Meta
+router.get("/", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (
+    mode === "subscribe" &&
+    token === config.VERIFY_TOKEN
+  ) {
+    console.log("Webhook Verified");
+    return res.status(200).send(challenge);
+  }
+
+  return res.sendStatus(403);
+});
+
+// استقبال الرسائل
+router.post("/", async (req, res) => {
+  console.log(
+    JSON.stringify(req.body, null, 2)
+  );
+
+  res.sendStatus(200);
+});
+
+export default router;
